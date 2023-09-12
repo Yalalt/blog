12eb2ad0ee111c0ff8271b4700d136a1f5a8445c
Yalalt <yalaltgandush@gmail.com>
2023-09-12 22:40:16 +0800

    0.0.1 version released

--- components/intro.js
+++ components/intro.js
@@ -1,21 +1,15 @@
-import { Media, Image } from "react-bootstrap";
+import { Media, Image } from 'react-bootstrap';
 
-export default () => (
-  <Media className="mb-4 admin-intro">
-    <Image
-      roundedCircle
-      width={64}
-      height={64}
-      className="mr-3"
-      src="logo.png"
-      alt="Generic placeholder"
-    />
-    <Media.Body>
-      <h5 className="font-weight-bold mb-0">1234.mn - Онлайн видео сургалт</h5>
-      <p className="welcome-text">
-        Бид програмчлалын технолгийн чиглэлээр төрөл бүрийн сонирхолтой
-        мэдээллүүдийг энэхүү блогоор хүргэж байна.
-      </p>
-    </Media.Body>
-  </Media>
-);
+export default function Intro() {
+  return (
+    <Media className='mb-4 admin-intro'>
+      <Image roundedCircle width={64} height={64} className='mr-3' src='logo.png' alt='Generic placeholder' />
+      <Media.Body>
+        <h5 className='font-weight-bold mb-0'>1234.mn - Онлайн видео сургалт</h5>
+        <p className='welcome-text'>
+          Бид програмчлалын технолгийн чиглэлээр төрөл бүрийн сонирхолтой мэдээллүүдийг энэхүү блогоор хүргэж байна.
+        </p>
+      </Media.Body>
+    </Media>
+  );
+}
